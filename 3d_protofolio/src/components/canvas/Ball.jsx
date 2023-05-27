2da7ac8d821306bdb11bcc60ac8f4989f395bad2
Lahiru Madushanka <lahirumadushandl@gmail.com>
2023-05-27 14:37:26 +0530

    Initial Commit

--- 3d_protofolio/src/components/canvas/Ball.jsx
+++ 3d_protofolio/src/components/canvas/Ball.jsx
@@ -11,6 +11,18 @@ const Ball = (props) => {
       <directionalLight position={[0,0,0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1,1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal 
+          position={[0,0,1]}
+          rotation={[2*Math.PI,0,6.25]}
+          flatShading
+          map={decal}
+          />
       </mesh>
 
     </Float>
@@ -33,4 +45,4 @@ const BallCanvas = ({icon}) => {
   )
 }
 
-export default Ball
+export default BallCanvas;
